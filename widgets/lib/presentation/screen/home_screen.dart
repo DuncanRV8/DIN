38ba71032968657e82d60d8ba00d63b2d0d13d2e
Duncan <duncanserpis@gmail.com>
2024-01-25 09:35:41 +0100

    new button_screen, car_screen

--- widgets/lib/presentation/screen/home_screen.dart
+++ widgets/lib/presentation/screen/home_screen.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:widgets/config/menu/menu_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return _HomeView();
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(
+        itemCount: appMenuItem.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _CustomList(menuItem: appMenuItem[index],);
+        },);
+  }
+}
+
+class _CustomList extends StatelessWidget {
+  final MenuItem menuItem;
+  const _CustomList({
+    super.key,
+    required this.menuItem
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    final esquemaColores = Theme.of(context).colorScheme;
+    return ListTile(
+      tileColor: esquemaColores.secondaryContainer,
+      title: Text(menuItem.titulo),
+      subtitle: Text(menuItem.subtitulo),
+      leading: Icon(menuItem.icono, color: esquemaColores.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: esquemaColores.primary),
+      onTap: () {
+      },
+    );
   }
 }
