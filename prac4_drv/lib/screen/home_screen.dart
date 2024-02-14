1816c7d78758894cd79bb7c76fbc2c07b1eed3ad
Duncan <duncanserpis@gmail.com>
2024-02-14 10:44:18 +0100

    NOt work

--- prac4_drv/lib/screen/home_screen.dart
+++ prac4_drv/lib/screen/home_screen.dart
@@ -1,57 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:prac4_drv/list/started_list.dart';
+import 'package:flutter/cupertino.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> nowPlayingMovies = [];
-  List<dynamic> upcomingMovies = [];
-
-  final MovieService _movieService = MovieService();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadMovies();
-  }
-
-  void _loadMovies() async {
-    try {
-      final nowPlaying = await _movieService.fetchNowPlayingMovies();
-      final upcoming = await _movieService.fetchUpcomingMovies();
-      setState(() {
-        nowPlayingMovies = nowPlaying;
-        upcomingMovies = upcoming;
-      });
-    } catch (e) {
-      print('Error loading movies: $e');
-    }
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Now Playing Movies: ${nowPlayingMovies.length}',
-            ),
-            Text(
-              'Upcoming Movies: ${upcomingMovies.length}',
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
